90342d5614d3d2611c7f2248d8a39e8a6d1f3d5a
Mariska-HoWest <mariska.de.backer@student.howest.be>
2025-09-08 09:37:18 +0200

    Load base dynamically

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ window.CONFIG = {
     API_KEY: 'AIzaSyBUtd0nI5kvhy0JjnpeXcSDdFTYYOVvxGM'
 };
 
+//Dynamic Base
+function SetBase()
+{
+    let base = document.createElement("base");
+    
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+        base.href = "/";
+    else
+        base.href = "/Hobby-Gallery/";
+
+    document.head.appendChild(base);
+}
+
 // Initialize Google API client
-function initGAPIClient(onReadyCallback) {
+function initGAPIClient(onReadyCallback) 
+{
     // Wait until gapi is loaded
     gapi.load('client:auth2', () => {
         gapi.client.init({
@@ -29,7 +43,8 @@ function initGAPIClient(onReadyCallback) {
 }
 
 // Shared helper: convert Google Drive share link to direct link
-function convertDriveLink(url) {
+function convertDriveLink(url) 
+{
     try {
         const match = url.match(/\/d\/([a-zA-Z0-9_-]+)/);  // Adjust regex to correctly capture file ID
         if (match && match[1]) {
