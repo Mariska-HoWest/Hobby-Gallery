88323c6534adf5497372af2b36048f2a0ea57bec
Mariska-HoWest <mariska.de.backer@student.howest.be>
2026-03-08 16:23:44 +0100

    generalize card css

--- kaarsen/DataManipulation.js
+++ kaarsen/DataManipulation.js
@@ -2,7 +2,6 @@
 window.initCandleManipulation = function (data) 
 {
   console.log('🟢 initCandleManipulation received:', Array.isArray(data) ? data.length : 0, 'rows');
-  console.log('🔍 First row:', Array.isArray(data) ? data[0] : null);
 
   UpdateCandleDisplay(data);
 };
@@ -12,19 +11,24 @@ function UpdateCandleDisplay(data)
   const display = document.getElementById('display');
 
   //Card creation
-data.forEach((candle =>
-  {
-    const card = document.createElement('div');
-    card.classList.add('card');
+  data.forEach((candle =>
+    {
+      const card = document.createElement('div');
+      card.classList.add('card');
 
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('img-container');
-    const img = document.createElement('img');
-    img.classList.add('img');
-    img.src = convertDriveLink(candle.Img);
-    imgContainer.appendChild(img);
-    card.appendChild(imgContainer);
-    display.appendChild(card);
-  }
-  ));
+      const title = document.createElement('div');
+      title.classList.add('title');
+      title.textContent = candle.Name;
+      card.appendChild(title);
+
+      const imgContainer = document.createElement('div');
+      imgContainer.classList.add('img-container');
+      const img = document.createElement('img');
+      img.classList.add('img');
+      img.src = convertDriveLink(candle.Img);
+      imgContainer.appendChild(img);
+      card.appendChild(imgContainer);
+
+      display.appendChild(card);
+    }));
 }
